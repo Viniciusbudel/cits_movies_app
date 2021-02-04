22ff5ddbdb190bbbac7f562867d3b8d4e39c891d
Viniciusbudel <vini_budel@hotmail.com>
2021-02-04 11:21:08 -0300

    ensure RemoteLoadMovies calls HttpClient with correct values

--- test/usecases/load_movies/remote_load_movies_test.dart
+++ test/usecases/load_movies/remote_load_movies_test.dart
@@ -1,20 +1,35 @@
 import 'dart:io';
 
+import 'package:cits_movie_app/data/http/http.dart';
+import 'package:faker/faker.dart';
+import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-class RemoteLoadSurveys{
-  Future<void> load() async{}
+class RemoteLoadMovies {
+  final String url;
+  final HttpClient httpClient;
+
+  RemoteLoadMovies({
+    @required this.url,
+    @required this.httpClient,
+  });
+
+  Future<void> load() async {
+    await httpClient.request(url: url, method: 'get');
+  }
 }
 
-class HttpClientSpy extends Mock implements HttpClient{}
+class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Should call HttpClient with correct values', () async{
-    final sut = RemoteLoadSurveys();
+  test('Should call HttpClient with correct values', () async {
+    final url = faker.internet.httpUrl();
+    final httpClient = HttpClientSpy();
+    final sut = RemoteLoadMovies(url: url, httpClient: httpClient);
 
     await sut.load();
 
-    verify('');
+    verify(httpClient.request(url: url, method: 'get'));
   });
 }
