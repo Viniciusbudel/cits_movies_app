bc3901a491fc5d8ac0e02b0c796bf90ef36b11fb
Viniciusbudel <vini_budel@hotmail.com>
2021-02-04 11:22:53 -0300

    move sut creation to the setup method

--- test/usecases/load_movies/remote_load_movies_test.dart
+++ test/usecases/load_movies/remote_load_movies_test.dart
@@ -23,11 +23,17 @@ class RemoteLoadMovies {
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Should call HttpClient with correct values', () async {
-    final url = faker.internet.httpUrl();
-    final httpClient = HttpClientSpy();
-    final sut = RemoteLoadMovies(url: url, httpClient: httpClient);
+  String url;
+  HttpClientSpy httpClient;
+  RemoteLoadMovies sut;
 
+  setUp((){
+     url = faker.internet.httpUrl();
+     httpClient = HttpClientSpy();
+     sut = RemoteLoadMovies(url: url, httpClient: httpClient);
+  });
+
+  test('Should call HttpClient with correct values', () async {
     await sut.load();
 
     verify(httpClient.request(url: url, method: 'get'));
