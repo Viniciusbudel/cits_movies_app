4a50502a8c816e6861a1026307c8ba2a3b7ec6bc
Viniciusbudel <vini_budel@hotmail.com>
2021-02-04 10:58:35 -0300

    add LoadMovies usecase

---
+++ lib/domain/usecases/load_movies.dart
@@ -0,0 +1,21 @@
+import 'package:meta/meta.dart';
+
+import '../../domain/entities/entities.dart';
+
+abstract class LoadMovies {
+  Future<List<MoviesEntity>> loadMovies(LoadMoviesParams params);
+}
+
+class LoadMoviesParams {
+  final String apiKey;
+  final String language;
+  final String page;
+  final String region;
+
+  LoadMoviesParams({
+    @required this.apiKey,
+    this.language,
+    this.page,
+    this.region,
+  });
+}
